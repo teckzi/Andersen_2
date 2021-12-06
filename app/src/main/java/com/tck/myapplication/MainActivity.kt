fcde754f224df903acab5bf21d42706f0fdbd565
teckzi <65223853+teckzi@users.noreply.github.com>
2021-12-06 18:12:53 +0300

    MainActivity added

--- app/src/main/java/com/tck/myapplication/MainActivity.kt
+++ app/src/main/java/com/tck/myapplication/MainActivity.kt
@@ -2,10 +2,36 @@ package com.tck.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.tck.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.LessonOnePointOne.setOnClickListener {
+
+        }
+
+        binding.LessonOnePointTwo.setOnClickListener {
+
+        }
+
+        binding.LessonOnePointThree.setOnClickListener {
+
+        }
+
+        binding.LessonOnePointFour.setOnClickListener {
+
+        }
+
+        binding.LessonTwoPointOne.setOnClickListener {
+
+        }
+
+        binding.LessonTwoPointThree.setOnClickListener {
+            
+        }
     }
 }

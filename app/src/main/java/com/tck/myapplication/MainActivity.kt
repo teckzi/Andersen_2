393938456c7aabb510c2dd894561d93bd13a9433
teckzi <65223853+teckzi@users.noreply.github.com>
2021-12-06 18:03:55 +0300

    Initial commit

---
+++ app/src/main/java/com/tck/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tck.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
